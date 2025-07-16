9e63ce53800e9852e454d2b99617214b806bee5c
khulelidzeliza <123889810+khulelidzeliza@users.noreply.github.com>
2025-07-16 15:12:24 +0400

    finished cocntroleer services of favoirte

--- ORAA/DTO/FavoriteDTO.cs
+++ ORAA/DTO/FavoriteDTO.cs
@@ -2,5 +2,11 @@
 {
     public class FavoriteDTO
     {
+        public string Items { get; set; }
+        public string Name { get; set; }
+        public bool IsPublic { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 }
